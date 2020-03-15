356533a7194540d9e59ce21dd9b520b8febe0fce
Conor Murphy <conor.p.murphy52@gmail.com>
2020-03-15 15:00:53 -0500

    got list comps for just the titles that runs much faster

--- sniped.py
+++ sniped.py
@@ -23,8 +23,16 @@ def num_movies(soup, url):
     countString = soup.findAll("h1", {"class": "section-heading"})[0].get_text()
     return(re.search(r'([\d]+)', countString).group())
 
-        
-# return dicts for each individual page
+       
+# basic calls that don't make hella requests, just gets film title
+def get_film_title(url):
+    pageText = requests.get(url)
+
+    pageSoup = BeautifulSoup(pageText.content, 'html.parser')
+
+    #makes a list of the alt text for every poster img, only way to get the titles from this url
+    return([poster.find('img', alt=True)['alt'] for poster in pageSoup.findAll('li', {'class': 'poster-container'})])
+# return dicts for each film on a given page 
 def get_film_info(url):
     
     pageText = requests.get(url)
@@ -47,9 +55,13 @@ def get_film_info(url):
         movies[title] = {'year': year, 'directors': directors, 'rating': rating, 'link': filmLink, 'image-src': img}
 
     return(movies)
-
+# unify paginations into a single list
+def unify_pages_list(url, pages):
+    # wild ass list comp here lmao
+    
+    return([a for x in range(1, pages+1) for a in get_film_title(url+f'/page/{x}')]) 
 # unify paginations into a single dict
-def unify_pages(url, pages):
+def unify_pages_dict(url, pages):
     movies = {}
 
     for x in range(1, pages+1):
@@ -58,18 +70,23 @@ def unify_pages(url, pages):
     return(movies)
 
 
-
+# call api to find the 
 def main():
     # create watchlist url 
     user = sys.argv[1]
     URL=f'https://letterboxd.com/{user}/watchlist'
+    COUNTRY='us'
+    SERVICES=['Netflix', 'Hulu', 'Amazon Prime Video']
 
     # main soup for basic info
     rawText = requests.get(URL)
     soup = BeautifulSoup(rawText.content, 'html.parser')
     pages = num_pages(soup)
     
-    print(unify_pages(URL, pages))
+    titles = unify_pages_list(URL, pages)
+
+    
+    
 # main execution
 if __name__ == '__main__':
     main()
