7d945ecdb7bad996049bb410cab33827c29c2df1
Conor Murphy <conor.p.murphy52@gmail.com>
2020-03-15 01:53:08 -0500

    handling pagination now

--- sniped.py
+++ sniped.py
@@ -18,19 +18,45 @@ soup = BeautifulSoup(rawText.content, 'html.parser')
 #find number of pages
 pages = len(list(soup.findAll("li", {"class": "paginate-page"})))
 
-#true soup calls template
-for x in range(1, pages+1):
-    print(URL+f'/page/{x}')
-
 
 #find the total number in watchlist
 countString = soup.findAll("h1", {"class": "section-heading"})[0].get_text()
 count = re.search(r'([\d]+)', countString).group()
 
+        
+# function to return dicts for each individual page
+def get_film_info(url):
+    
+    pageText = requests.get(url)
 
-#grabbing all movies 
-postersObj = soup.findAll("li", {"class": "poster-container"})
-posters = list(postersObj)
+    pageSoup = BeautifulSoup(pageText.content, 'html.parser')
+    
+    movies = {}
+
+    for poster in pageSoup.findAll("li", {"class": "poster-container"}):
+        title = poster.find('img', alt=True)['alt']
+        filmLink = f'https://letterboxd.com' + poster.find('div', {'class': 'poster film-poster really-lazy-load'})['data-target-link']
+        img = poster.find('img')['src']
+
+        movies[title] = {'link': filmLink, 'image-src': img}
+
+    return(movies)
+
+#function to unify paginations into a single dict
+def unify_pages(url, pages):
+    movies = {}
+
+    for x in range(1, pages+1):
+        movies.update(get_film_info(url+f'/page/{x}'))
+
+    return(movies)
+
+print(unify_pages(URL, pages))
+
+#for x in range(1, pages+1):
+#    print(get_film_info(URL+f'/page/{x}'))
+
+
 
 
 
