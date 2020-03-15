437a0221470e7f2de06a983629f34cf281dfccfe
Conor Murphy <conor.p.murphy52@gmail.com>
2020-03-15 17:46:13 -0500

    mvp first version ???

--- sniped.py
+++ sniped.py
@@ -69,14 +69,60 @@ def unify_pages_dict(url, pages):
 
     return(movies)
 
+# first pass to cross reference with API
+def reference_API(titles, COUNTRY, SERVICES, apiURL, HEADERS):
+    referenced_list = {service: [] for service in SERVICES}
+    for title in titles:
+        querystring = {'term': title, 'country': COUNTRY}
+        response = json.loads(requests.request('GET', apiURL, headers=HEADERS, params=querystring).text)
+        for name in response['results']:
+            for result in name['locations']:
+                if result['display_name'] in SERVICES:
+                    referenced_list[result['display_name']].append(title)
+    return(referenced_list)
 
+# yield tuples of overlap
+def scheme_API(titles, COUNTRY, SERVICES, apiURL, HEADERS):
+    referenced_list = {service: [] for service in SERVICES}
+    for title in titles:
+        querystring = {'term': title, 'country': COUNTRY}
+        response = json.loads(requests.request('GET', apiURL, headers=HEADERS, params=querystring).text)
+        for name in response['results']:
+            movie = name['name']
+            if name['name'] == title:
+                for result in name['locations']:
+                    if result['display_name'] in SERVICES:
+                        yield result['display_name'], movie
+# yield dictionary per streaming service
+def make_dict(titles, COUNTRY, SERVICES, apiURL, HEADERS):
+    big_dict = {service: [] for service in SERVICES}
+
+    for twosome in scheme_API(titles, COUNTRY, SERVICES, apiURL, HEADERS):
+        if twosome[1] not in big_dict[twosome[0]]:
+            big_dict[twosome[0]].append(twosome[1])
+    
+    return(big_dict)
+
+# format output
+def printout(big_dict):
+    for service, movies in big_dict.items():
+        print(service + ': ' + ', '.join(movies)) 
+        
 # call api to find the 
 def main():
     # create watchlist url 
     user = sys.argv[1]
     URL=f'https://letterboxd.com/{user}/watchlist'
+    
+    # api info:
+    apiURL='https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup'
     COUNTRY='us'
     SERVICES=['Netflix', 'Hulu', 'Amazon Prime Video']
+
+    HEADERS={
+    'x-rapidapi-host': "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+    'x-rapidapi-key': "5f41bc51e0mshfc2f106a7727246p155c22jsn37fd261cb5e0"
+    }
 
     # main soup for basic info
     rawText = requests.get(URL)
@@ -84,17 +130,14 @@ def main():
     pages = num_pages(soup)
     
     titles = unify_pages_list(URL, pages)
+    
+    big_dict = make_dict(titles, COUNTRY, SERVICES, apiURL, HEADERS)
 
-    
-    
+    printout(big_dict)
+
 # main execution
 if __name__ == '__main__':
     main()
 
     
-
-
-
-
-
 
