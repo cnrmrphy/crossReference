e3cc1450cfe71984e3f541c18ab1f491494c916c
Conor Murphy <conor.p.murphy52@gmail.com>
2020-03-17 02:55:08 -0500

    setting myself up to have manual entry of providers with close matches

--- config_files/config.py
+++ config_files/config.py
@@ -7,10 +7,10 @@ import geopy
 import os
 import json
 import inquirer
+from providers import load_providers
 
 
-
-def add_country(filename='config_files/config.json'):
+def add_country(filename):
     loc = geocoder.ip('me').latlng
     
     code = requests.get(f'http://api.geonames.org/countryCode?lat={loc[0]}&lng={loc[1]}&username=steemer').text.strip()
@@ -24,8 +24,8 @@ def add_country(filename='config_files/config.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
 
-
-def add_user(filename='config_files/config.json'):
+#def load_providers()
+def add_user(filename):
     
     username = input('Enter your Letterboxd username: ')
 
@@ -38,7 +38,7 @@ def add_user(filename='config_files/config.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
 
-def add_services(filename='config_files/config.json'):
+def add_services(filename):
     serviceQ = [
         inquirer.Checkbox('SERVICES',
             message='Select all the services you would like to search',
@@ -57,7 +57,7 @@ def add_services(filename='config_files/config.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
 
-def clear_config(filename='config_files/config.json'):
+def clear_config(filename):
     with open(filename, 'r') as f:
         data = json.load(f)
         for key in data:
@@ -73,12 +73,19 @@ def clear_config(filename='config_files/config.json'):
 
 
 def main():
-    configData = json.loads(open('config_files/config.json').read())
+    filename = 'config.json'
+    configData = json.loads(open(filename).read())
+   
     if not configData["USER"]:
-        add_user()
-    add_country()
+        add_user(filename)
+  
+    add_country(filename)
+    configData = json.loads(open(filename).read())
+    load_providers.main()
+    
+    
     if not configData["SERVICES"]:
-        add_services()
+        add_services(filename)
     
 
 if __name__ == '__main__':
--- config_files/providers/load_providers.py
+++ config_files/providers/load_providers.py
@@ -5,12 +5,14 @@ import requests
 import re
 import json
 from justwatch import JustWatch
-
+from os import path
 
 
 # return the object that contains raw info about all providers
 def search_object():
-    country = json.loads(open('../config_files/config.json').read())["COUNTRY"]
+    basepath = path.dirname(__file__)
+    filepath = path.abspath(path.join(basepath, "..", "..", "config_files", "config.json"))
+    country = json.loads(open(filepath).read())["COUNTRY"]
     just_watch=JustWatch(country=country)
 
     return(just_watch.get_providers())
@@ -18,15 +20,19 @@ def search_object():
 # load a json file with a map of provider ids to provider names
 def load_idData(providers):
     idData = {str(provider['id']): provider['clear_name'] for provider in providers}
-
-    with open('idData.json', 'w') as file:
-        json.dump(idData, file)
+    filename = "idData.json"
+    basepath = path.dirname(__file__)
+    filepath = path.abspath(path.join(basepath, "..", "providers", filename))
+    with open(filepath, 'w') as file:
+       json.dump(idData, file)
 
 def load_provData(providers):
     providerData = {provider['clear_name']: str(provider['id']) for provider in providers}
-
-    with open('providerData.json', 'w') as file:
-        json.dump(providerData, file)
+    filename = "providerData.json"
+    basepath = path.dirname(__file__)
+    filepath = path.abspath(path.join(basepath, "..", "providers", filename))
+    with open(filepath, 'w') as file:
+       json.dump(providerData, file)
 def main():
     providers = search_object() 
 
